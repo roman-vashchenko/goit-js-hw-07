f36b8d1404d2e3f4fe786ccc02aab1309fa30bfb
Roman Vashchenko <roman1991dp@gmail.com>
2023-01-25 22:44:25 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js"
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+const gallagyImages = document.querySelector(".gallery")
+
+function createGallaryMarkup(items) {
+  return items
+    .map(
+      (item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+    )
+    .join("")
+}
+
+const imgMarkup = createGallaryMarkup(galleryItems)
+
+gallagyImages.innerHTML = imgMarkup
+
+gallagyImages.addEventListener("click", onImgClick)
+
+// gallagyImages.addEventListener("keydown", (event) => {
+//   if (event.code === "Escape") {
+//     instance.close()
+//   }
+// })
+
+function onImgClick(event) {
+  event.preventDefault()
+
+  if (event.target.nodeName !== "IMG") {
+    return
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+  instance.show()
+  gallagyImages.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close()
+    }
+  })
+}
