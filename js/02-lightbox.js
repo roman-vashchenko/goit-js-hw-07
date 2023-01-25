18e01aa2192e2bd4c063d18b00b6ab73ff40a2af
Roman Vashchenko <roman1991dp@gmail.com>
2023-01-25 23:30:06 +0200

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js"
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+const gallagyImages = document.querySelector(".gallery")
+
+function createGallaryMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `<a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt = "${item.description} "title="${item.description}" />
+          </a>`
+    )
+    .join("")
+}
+
+const imgMarkup = createGallaryMarkup(galleryItems)
+
+gallagyImages.innerHTML = imgMarkup
+
+gallagyImages.addEventListener("click", onImgClick)
+
+function onImgClick(event) {
+  event.preventDefault()
+  if (event.target.nodeName !== "IMG") {
+    return
+  }
+  const lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 })
+}
